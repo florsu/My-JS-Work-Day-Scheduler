75a7f6ce7c5b4c15581275a775452ae218fe9f8c
Christian Flores <christiangflores0824@gmail.com>
2021-08-11 00:17:47 -0700

    add save icon and create eventlistener

--- js/timeblocks.js
+++ js/timeblocks.js
@@ -1,10 +1,17 @@
-
+for (let i = 1; i < 10; i++) {
+    const btn = `btn${i}`
+    const btnEle = document.getElementById(btn)
+    btnEle.addEventListener('click', btnListener)
+}
+function btnListener() {
+    console.log(this)
+}
 hourlyBlock()
 function hourlyBlock() {
     const currentHour = DateTime.now().toFormat('H')
     //const currentHour = 10
     for (let i = 1; i < 10; i++) {
-        const rowHour = i+8
+        const rowHour = i + 8
         const row = `row${i}`
         const rowEle = document.getElementById(row)
         rowEle.classList.remove('past')
@@ -12,18 +19,27 @@ function hourlyBlock() {
         rowEle.classList.remove('future')
         if (rowHour < currentHour) {
             rowEle.classList.add('past')
-            console.log(row +' ' + rowHour +'<'+currentHour +' past')
+            console.log(row + ' ' + rowHour + '<' + currentHour + ' past1')
+            console.log(`${row} ${rowHour}<${currentHour} past2`)
         }
         if (rowHour == currentHour) {
             rowEle.classList.add('present')
-            console.log(row +' ' + rowHour +'=='+currentHour +' present')
+            console.log(row + ' ' + rowHour + '==' + currentHour + ' present1')
+            console.log(`${row} ${rowHour}==${currentHour} present2`)
         }
         if (rowHour > currentHour) {
             rowEle.classList.add('future')
-            console.log(row +' ' + rowHour +'>'+currentHour +' future')
+            console.log(row + ' ' + rowHour + '>' + currentHour + ' future1')
+            console.log(`${row} ${rowHour}>${currentHour} future2`)
         }
-        
+
     }
 }
 
 // `Earth is estimated to be ${age} billion years old.`
+console.log('1 hello world')
+console.log('2 h' + 'e' + 'l' + 'l' + 'o' + ' ' + 'world')
+const h = 'hello'
+const w = 'world'
+console.log('3 hello world ${h} ${w}')
+console.log(`4 hello world ${h} ${w}`)
